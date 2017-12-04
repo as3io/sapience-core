fa5ee49eadd858e3772f3133f74867ad87d7a392
Jacob Bare <jacob@limit0.io>
2017-12-04 14:12:11 -0600

    Move common APP_NAME to global

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,9 @@
+const APP_NAME = 'Sapience';
 module.exports = Object.freeze({
-  APP_NAME: 'Sapience',
+  APP_NAME,
   DEFAULT_TRACKER_NAME: '_default_',
-  WINDOW_VAR_NAME: 'SapienceObject',
+  WINDOW_VAR_NAME: `${APP_NAME}Object`,
   STORAGE_PREFIX: '__sapi_',
   DB_PREFIX: 'sapi',
-  CLIENT_KEY_HEADER_NAME: 'X-Sapience-ClientKey',
+  CLIENT_KEY_HEADER_NAME: `X-${APP_NAME}-ClientKey`,
 });
