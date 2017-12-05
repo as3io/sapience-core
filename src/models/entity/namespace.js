e041368205d50bbef1371dacecd0f1e7a8aeca7b
Jacob Bare <jacob@limit0.io>
2017-12-05 13:32:09 -0600

    Add entity namespace model

---
+++ src/models/entity/namespace.js
@@ -0,0 +1,214 @@
+const compose = require('stampit');
+const { dasherized } = require('../../utils/cast-as');
+const SelfAware = require('../../composables/self-aware');
+const ValuesProxy = require('../../composables/values-proxy');
+
+const { assign } = Object;
+
+const Namespace = compose({
+  /**
+   *
+   * @param {Object} opts
+   * @param {string} opts.z The namespace zone (tenant/org name).
+   * @param {string} opts.b The namespace base (grouping name).
+   * @param {string} opts.n The namespace name (model name).
+   */
+  init({ z, b, n } = {}) {
+    this.z = z;
+    this.b = b;
+    this.n = n;
+  },
+
+  /**
+   * Defines property descriptors for this instance.
+   * Allows for getters/setters and other options.
+   * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty}
+   */
+  propertyDescriptors: {
+    b: { enumerable: true, set(v) { return this.setBase(v); }, get() { return this.getBase(); } },
+    n: { enumerable: true, set(v) { return this.setName(v); }, get() { return this.getName(); } },
+    z: { enumerable: true, set(v) { return this.setZone(v); }, get() { return this.getZone(); } },
+  },
+
+  /**
+   * Instance methods.
+   */
+  methods: {
+    /**
+     * Determines if this is an empty instance (missing the zone, base, and name).
+     *
+     * @return {boolean}
+     */
+    isEmpty() {
+      return Boolean(!this.z && !this.b && !this.n);
+    },
+
+    /**
+     * Validates the namespace.
+     * At minimum, a `name` (`n`) must be specified
+     *
+     * @return {boolean}
+     */
+    isValid() {
+      return Boolean(this.n);
+    },
+
+    /**
+     * Gets a namespace property value.
+     *
+     * @param {string} name
+     * @return {(string|undefined)}
+     */
+    get(name) {
+      return this.getValue(name);
+    },
+
+    /**
+     * Gets the base (`b`) value of the entity namespace.
+     *
+     * @return {(string|undefined)}
+     */
+    getBase() {
+      return this.get('b');
+    },
+
+    /**
+     * Gets the name (`n`) value of the entity namespace.
+     *
+     * @return {(string|undefined)}
+     */
+    getName() {
+      return this.get('n');
+    },
+
+    /**
+     * Gets the zone (`z`) value of the entity namespace.
+     *
+     * @return {(string|undefined)}
+     */
+    getZone() {
+      return this.get('z');
+    },
+
+    /**
+     * Prepares an entity namespace property value for setting.
+     *
+     * @param {*} value
+     * @return {(string|undefined)}
+     */
+    prepare(value) {
+      const cast = dasherized(value);
+      return cast.length ? cast : undefined;
+    },
+
+    /**
+     * Sets an entity namespace property value.
+     *
+     * @param {string} name
+     * @param {*} value
+     * @return {this}
+     */
+    set(name, value) {
+      this.setValue(name, this.prepare(value));
+      return this;
+    },
+
+    /**
+     * Sets the base (`b`) value of the entity namespace.
+     *
+     * @param {string} base
+     * @return {this}
+     */
+    setBase(base) {
+      return this.set('b', base);
+    },
+
+    /**
+     * Sets the name (`n`) value of the entity namespace.
+     *
+     * @param {string} name
+     * @return {this}
+     */
+    setName(name) {
+      return this.set('n', name);
+    },
+
+    /**
+     * Sets the zone (`z`) value of the entity namespace.
+     *
+     * @param {string} zone
+     * @return {this}
+     */
+    setZone(zone) {
+      return this.set('z', zone);
+    },
+
+    /**
+     * Converts the namespace to a string.
+     *
+     * Given the following object properties:
+     * 1. `{ n: undefined, b: undefined, n: 'bar' }` returns `..bar`.
+     * 2. `{ n: undefined, b: 'foo', n: 'bar' }` returns `.foo.bar`.
+     * 3. `{ n: 'baz', b: 'foo', n: 'bar' }` returns `baz.foo.bar`.
+     * 4. `{ n: 'baz', b: undefined, n: 'bar' }` returns `baz..bar`.
+     * 5. `{ n: null, b: undefined, n: undefined }` returns `''`.
+     * 6. `{ n: '', b: 'bar', n: '' }` returns `.bar.`.
+     * 7. `{ n: 'baz', b: null, n: '' }` returns `baz..`.
+     *
+     * @return {string}
+     */
+    toString() {
+      if (this.isEmpty()) return '';
+      return [this.z, this.b, this.n].join('.');
+    },
+  },
+
+  /**
+   * Static methods.
+   */
+  statics: {
+    /**
+     * Converts a stringified namsespace into a Namespace object.
+     *
+     * Given the following string:
+     * 1. `bar` returns `{ n: undefined, b: undefined, n: 'bar' }`.
+     * 2. `foo.bar` returns `{ n: undefined, b: 'foo', n: 'bar' }`.
+     * 3. `baz.foo.bar` returns `{ n: 'baz', b: 'foo', n: 'bar' }`.
+     * 4. `baz..bar` returns `{ n: 'baz', b: undefined, n: 'bar' }`.
+     *
+     * All non-string values of `v` will return an empty Namespace.
+     *
+     * @param {string} v The stringified namespace.
+     * @return {Namespace}
+     */
+    fromString(v) {
+      const args = typeof v === 'string' ? v.split('.') : [];
+      return Namespace.make(...args);
+    },
+
+    /**
+     * Makes a new Namspace instance with the provided namespace parameters.
+     * Can be partially specified, e.g.:
+     * 1. make(name)
+     * 2. make(base, name)
+     * 3. make(zone, base, name)
+     * 4. make(zone, null, name)
+     *
+     * @return {Namespace}
+     */
+    make(...args) {
+      const map = { 0: 'z', 1: 'b', 2: 'n' };
+      while (args.length < 3) {
+        args.unshift(undefined);
+      }
+      const opts = args.reduce((obj, val, idx) => {
+        const key = map[idx];
+        if (key) assign(obj, { [key]: val });
+        return obj;
+      }, {});
+      return Namespace(opts);
+    },
+  },
+}, ValuesProxy, SelfAware);
+
+module.exports = Namespace;
---
+++ test/models/entity/namespace.spec.js
@@ -0,0 +1,200 @@
+const expect = require('chai').expect;
+const Namespace = require('../../../src/models/entity/namespace');
+
+const testInstance = (ns) => {
+  expect(ns).to.be.a('object');
+  expect(ns).to.respondTo('getStamp');
+  expect(ns.getStamp()).to.equal(Namespace);
+};
+
+const testStaticMethod = (method) => {
+  expect(Namespace[method]).to.be.a('function');
+};
+
+const testGetters = (ns, exp) => {
+  expect(ns.n).to.equal(exp.n);
+  expect(ns.z).to.equal(exp.z);
+  expect(ns.b).to.equal(exp.b);
+
+  expect(ns.get('n')).to.equal(exp.n);
+  expect(ns.get('z')).to.equal(exp.z);
+  expect(ns.get('b')).to.equal(exp.b);
+
+  expect(ns.getName()).to.equal(exp.n);
+  expect(ns.getZone()).to.equal(exp.z);
+  expect(ns.getBase()).to.equal(exp.b);
+};
+
+const testSetters = (init, exp) => {
+  const ns1 = Namespace();
+  ns1.n = init.n;
+  ns1.z = init.z;
+  ns1.b = init.b;
+  testGetters(ns1, exp);
+  const ns2 = Namespace();
+  ns2.set('n', init.n);
+  ns2.set('z', init.z);
+  ns2.set('b', init.b);
+  testGetters(ns2, exp);
+  const ns3 = Namespace();
+  ns3.setName(init.n);
+  ns3.setZone(init.z);
+  ns3.setBase(init.b);
+  testGetters(ns3, exp);
+}
+
+/**
+ * Given { z, b, n }
+ * Combinations:
+ * z
+ * b
+ * n
+ * z b
+ * z n
+ * b n
+ * z b n
+ */
+const cases = [
+  {
+    init: { z: 'foo', b: 'bar', n: 'baz' },
+    args: ['foo', 'bar', 'baz'],
+    stringified: 'foo.bar.baz',
+    expected: { z: 'foo', b: 'bar', n: 'baz', isEmpty: false, isValid: true },
+  },
+  {
+    init: { b: 'bar', z: null, n: undefined },
+    args: [null, 'bar', null],
+    stringified: '.bar.',
+    expected: { z: undefined, b: 'bar', n: undefined, isEmpty: false, isValid: false },
+  },
+  {
+    init: { z: 'foo', b: '' },
+    args: ['foo', '', null],
+    stringified: 'foo..',
+    expected: { z: 'foo', b: undefined, n: undefined, isEmpty: false, isValid: false },
+  },
+  {
+    init: { z: 'foo', n: 'bar' },
+    args: ['foo', undefined, 'bar'],
+    stringified: 'foo..bar',
+    expected: { z: 'foo', b: undefined, n: 'bar', isEmpty: false, isValid: true },
+  },
+  {
+    init: { z: 'foo', b: 'bar' },
+    args: ['foo', 'bar', ''],
+    stringified: 'foo.bar.',
+    expected: { z: 'foo', b: 'bar', n: undefined, isEmpty: false, isValid: false },
+  },
+  {
+    init: { n: 'bar' },
+    args: ['bar'],
+    stringified: '..bar',
+    expected: { z: undefined, b: undefined, n: 'bar', isEmpty: false, isValid: true },
+  },
+  {
+    init: { b: 'bar', n: 'baz' },
+    args: [null, 'bar', 'baz'],
+    stringified: '.bar.baz',
+    expected: { z: undefined, b: 'bar', n: 'baz', isEmpty: false, isValid: true },
+  },
+  {
+    init: {},
+    args: [null, undefined, '', 'foo'],
+    stringified: '',
+    expected: { z: undefined, b: undefined, n: undefined, isEmpty: true, isValid: false },
+  },
+];
+
+describe('models/entity/namespace', function() {
+  it('should return a factory function', function(done) {
+    expect(Namespace).to.be.a('function');
+    done();
+  });
+  describe('#()', function() {
+    it('it should be a Namespace instance', function(done) {
+      testInstance(Namespace());
+      done();
+    });
+    cases.forEach((c, i) => {
+      it(`should create the Namespace with the expected values for case ${i}`, function(done) {
+        const { init, expected } = c;
+        const ns = Namespace(init);
+        testGetters(ns, expected);
+        done();
+      });
+      it(`should properly set values to the Namespace for case ${i}`, function(done) {
+        const { init, expected } = c;
+        testSetters(init, expected);
+        done();
+      });
+    });
+  });
+  describe('#make()', function() {
+    it('should be a static function', function(done) {
+      testStaticMethod('make');
+      done();
+    });
+    it('should return a Namespace object instance', function(done) {
+      const ns = Namespace.make();
+      testInstance(ns);
+      done();
+    });
+    cases.forEach((c, i) => {
+      it(`should create the Namespace with the expected values for case ${i}`, function(done) {
+        const { args, expected } = c;
+        const ns = Namespace.make(...args);
+        testGetters(ns, expected);
+        done();
+      });
+    });
+  });
+  describe('#fromString()', function() {
+    it('should be a static function', function(done) {
+      testStaticMethod('fromString');
+      done();
+    });
+    it('should return a Namespace object instance', function(done) {
+      const ns = Namespace.fromString();
+      testInstance(ns);
+      done();
+    });
+    cases.forEach((c, i) => {
+      it(`should create the Namespace with the expected values for case ${i}`, function(done) {
+        const { stringified, expected } = c;
+        const ns = Namespace.fromString(stringified);
+        testGetters(ns, expected);
+        done();
+      });
+    });
+  });
+  describe('#toString()', function() {
+    cases.forEach((c, i) => {
+      const { stringified, init, args } = c;
+      it(`should create correct stringified value for case ${i}`, function(done) {
+        [Namespace(init),  Namespace.fromString(stringified), Namespace.make(...args)]
+          .forEach(ns => expect(ns.toString()).to.equal(stringified));
+        done();
+      });
+    });
+  });
+  describe('#isValid()', function() {
+    cases.forEach((c, i) => {
+      const { stringified, init, args, expected } = c;
+      it(`should return the correct value for case ${i}`, function(done) {
+        [Namespace(init), Namespace.fromString(stringified), Namespace.make(...args)]
+          .forEach(ns => expect(ns.isValid()).to.equal(expected.isValid));
+        done();
+      });
+    });
+  });
+  describe('#isEmpty()', function() {
+    cases.forEach((c, i) => {
+      const { stringified, init, args, expected } = c;
+      it(`should return the correct value for case ${i}`, function(done) {
+        [Namespace(init), Namespace.fromString(stringified), Namespace.make(...args)]
+          .forEach(ns=> expect(ns.isEmpty()).to.equal(expected.isEmpty));
+        done();
+      });
+    });
+  });
+});
